64ec70695382b232f9cec392f0c40fdb411f5eca
Erick Danzer <edanzer@gmail.com>
2021-09-21 21:42:21 -0600

    Refactored class-based code to functional components with hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import PropertyRegistryContract from "./contracts/PropertyRegistry.json";
 import getWeb3 from "./getWeb3";
 import Button from '@mui/material/Button';
@@ -6,88 +6,82 @@ import TextField from '@mui/material/TextField';
 
 import "./App.css";
 
-class App extends Component {
-  state = { 
-    property: '',
-    currentOwner: '',
-    newOwner: '',
-    ownerList: [],
-    web3: null, 
-    accounts: null, 
-    contract: null, 
-  };
+const App = () => {
+  const [property, setProperty] = useState('');
+  const [owner, setOwner] = useState('');
+  const [newOwner, setNewOwner] = useState('');
+  const [ownerList, setOwnerList] = useState([]);
+  const [web3, setWeb3] = useState(null);
+  const [accounts, setAccounts] = useState(null);
+  const [contract, setContract] = useState(null);
 
-  componentDidMount = async () => {
-    try {
-    
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+  useEffect(() => {
+    async function connectToSmartContract() {
+      try {
+        // Get network provider and web3 instance.
+        const web3Temp = await getWeb3();
+  
+        // Use web3 to get user accounts.
+        const accountsTemp = await web3Temp.eth.getAccounts();
+  
+        // Get contract instance.
+        const networkId = await web3Temp.eth.net.getId();
+        const deployedNetwork = PropertyRegistryContract.networks[networkId];
+        const instance = new web3Temp.eth.Contract(
+          PropertyRegistryContract.abi,
+          deployedNetwork && deployedNetwork.address,
+        );
 
-      // Use web3 to get user accounts.
-      const accounts = await web3.eth.getAccounts();
-
-      // Get contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = PropertyRegistryContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        PropertyRegistryContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
-
-      // Set web3, accounts, and contract to state and get property details.
-      this.setState({ web3, accounts, contract: instance }, this.getPropertyDetails);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
+        // Update state for web3, accounts, contract
+        setWeb3(web3Temp);
+        setAccounts(accountsTemp);
+        setContract(instance);
+      } catch (error) {
+        alert(`Failed to load web3, accounts, or contract. Check console for details.`);
+        console.error(error);
+      }
     }
-  };
+    connectToSmartContract();
+	}, []);
 
-  getPropertyDetails = async () => {
-    const { accounts, contract } = this.state;
+  useEffect(() => {
+    if(contract) getPropertyDetails();
+	}, [contract]);
 
+  const getPropertyDetails = async () => {
     // Get current property details from the contract.
     const property = await contract.methods.getProperty().call();
-    const currentOwner = await contract.methods.getCurrentOwner().call();
+    const owner = await contract.methods.getCurrentOwner().call();
     const condensedOwnerList = await contract.methods.getOwnerList().call();
     let ownerList = condensedOwnerList.map(item => item + " | ");
 
-    // Update state
-    this.setState({ 
-      property,
-      currentOwner,
-      ownerList
-    });
+    // Update state for property and ownership
+    setProperty(property);
+    setOwner(owner);
+    setOwnerList(ownerList);
   };
 
-  updateOwner = async () => {
-    const { accounts, contract } = this.state;
-
+  const updateOwner = async () => {
     // Send request to update owner with contract.
-    await contract.methods.changeOwner(this.state.newOwner).send({ from: accounts[0] });
+    await contract.methods.changeOwner(newOwner).send({ from: accounts[0] });
 
     // Update property details 
-    this.getPropertyDetails();
+    setTimeout(getPropertyDetails(), 3000);
   };
 
-  render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
-    return (
+  return(
+    !web3 ? <div>Loading Web3, accounts, and contract...</div> : (
       <div className="App">
         <h1>Property Details</h1>
-        <p>Property: {this.state.property}</p>
-        <p>Current Owner: {this.state.currentOwner}</p>
-        <p>Owner List: {this.state.ownerList}</p>
-        <TextField id="new-owner" label="Enter new owner" value={this.state.newOwner} onChange={(e) => this.setState({newOwner: e.target.value})} variant="outlined" />
-        <Button variant="contained" size="large" onClick={this.updateOwner}>Submit Ownership Change</Button>
-        <Button variant="contained" onclick={this.getPropertyDetails}>Refresh Property Details</Button>
+        <p>Property: {property}</p>
+        <p>Current Owner: {owner}</p>
+        <p>Owner List: {ownerList}</p>
+        <TextField id="new-owner" label="Enter new owner" value={newOwner} onChange={(e) => setNewOwner(e.target.value)} variant="outlined" />
+        <Button variant="contained" size="large" onClick={updateOwner}>Submit Ownership Change</Button>
+        <Button variant="contained" onClick={getPropertyDetails}>Refresh Property Details</Button>
       </div>
-    );
-  }
+    )
+  ); 
 }
 
 export default App;
